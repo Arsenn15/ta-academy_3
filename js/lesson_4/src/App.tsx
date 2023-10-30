222e8882945f979a4c4dc9958329343875c3b037
Arsen Sahakyan <sahakyan.9788@gmail.com>
2023-10-30 11:53:40 +0400

    homevork 4 done

--- js/lesson_4/src/App.tsx
+++ js/lesson_4/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import {arraySum, arrayFilter, getUserNames} from './utils';
+import { arraySum, arrayFilter, getUserNames } from './utils';
+import { personNames } from "./task";
+
 
 function App() {
     const array = [1, 2, 3, 4, 5];
@@ -20,14 +22,23 @@ function App() {
     const userNamesArray = getUserNames(usersArray);
 
     return (
-    <div className="App">
-        <div className="wrapper">
-            <p>{sum}</p>
-            <p>{filter.toString()}</p>
-            <p>{userNamesArray.toString()}</p>
+        <div className="App">
+            <div className="wrapper">
+                <p>{sum}</p>
+                <p>{filter.toString()}</p>
+                <p>{userNamesArray.toString()}</p>
+                <p>{personNames}</p>
+                {/*{*/}
+                {/*    personNames.map((item:string, index: number) => {*/}
+                {/*        return <p*/}
+                {/*            key={index}>*/}
+                {/*            {item}*/}
+                {/*        </p>*/}
+                {/*    })*/}
+                {/*}*/}
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
